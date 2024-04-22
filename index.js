e24c50a6c81e87d2a563ece933b3946c8e5aed43
Brock Britton <brockbritton@outlook.com>
2024-04-22 14:34:12 -0700

    edits for server

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const cors = require('cors');
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
